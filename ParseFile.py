89a51cef18a8945c19aa821c9ad83ad402a30ad2
johnnyc2 <johnnyc2@gmail.com>
2017-02-09 16:04:09 +1100

    Sorting, cleaning complete. Inserting '\n' in progress.

--- ParseFile.py
+++ ParseFile.py
@@ -5,11 +5,27 @@ def removeValuesFromList(listName, valueToRemove):
 
 def main():
     linesList = open('D90Clubs2016.txt').read().splitlines()
-    print(linesList)
     linesList = [x for x in linesList if x != '']
+    newAreaIndex = []
     print(linesList)
     for x in linesList:
-        if x.startswith('Page No')
+        if x.startswith('Page No.'):
+            newAreaIndex.append(linesList.index(x))
+            i = 3
+            while i < 9:
+                linesList.insert(linesList.index(x)+i,'.')
+                i += 1
+    j = 9
+    k = 0
+    while j < len(linesList):
+        linesList.insert(j, '\n')
+        k += 1
+        print(k)
+        j += 9 + k
+        print(j)
+
+    print(newAreaIndex)
+    print(linesList)
     return
 
 if __name__ == "__main__":
